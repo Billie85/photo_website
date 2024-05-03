2cf8b8e4a9d19a536a56ca07bf13ba74a1d78092
Kimura Ayumi <kayumi@c4r1s11.42tokyo.jp>
2024-05-03 16:36:01 +0900

    レスポンシブ流対応の不備の調節とデザイン追加

--- src/Tablet/Tablet.tsx
+++ src/Tablet/Tablet.tsx
@@ -3,33 +3,31 @@ import styles from './Tablet.module.css';
 import image from '../image/image.png';
 
 
-const Tablet: FunctionComponent = () => {
-  return (
-    <div className={styles.tablet}>
-      <div className={styles.tablet1}>
-        <div className={styles.backGround} />
-        <img className={styles.mainImageIcon} alt="" src={image} />
-        <div className={styles.ayumiKimura}>Ayumi Kimura</div>
-        <div className={styles.imageInfo}>
-          <div className={styles.imageExplainText}>
-            <div className={styles.circleRight} />
-            <div className={styles.textAboutImage}>Rising from the heart of the Tularosa Basin is one of the world's great natural wonders - the glistening white sands of New Mexico</div>
-            <div className={styles.textPhotographer}>photo by Carly Osborn</div>
-          </div>
-        </div>
-        <div className={styles.myInfo}>
-          <div className={styles.smallCircle} />
-          <div className={styles.leftCircle} />
-          <div className={styles.aboutMe}>
-            <span className={styles.aboutMeTxtContainer}>
-              <p className={styles.helloImAyumi}>Hello, I'm Ayumi Kimura. As a frontend engineer, I'm passionate about web development.</p>
-              <p className={styles.helloImAyumi}> I particularly enjoy crafting user experiences while staying abreast of the latest technologies and design trends.</p>
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+
+const Tablet:FunctionComponent = () => {
+	return (
+	<div className={styles.tablet}>
+		<div className={styles.backGround} />
+		<img className={styles.mainImageIcon} alt="" src={image} />
+		<div className={styles.ayumiKimura}>Ayumi Kimura</div>
+		<div className={styles.imageInfo}>
+			<div className={styles.imageExplainText}>
+				<div className={styles.circleRight} />
+				<div className={styles.textAboutImage}>Rising from the heart of the Tularosa Basin is one of the world's great natural wonders - the glistening white sands of New Mexico</div>
+				<div className={styles.textPhotographer}>photo by Carly Osborn</div>
+			</div>
+		</div>
+		<div className={styles.myInfo}>
+			<div className={styles.smallCircle} />
+			<div className={styles.leftCircle} />
+			<div className={styles.aboutMe}>
+				<span>
+					<p className={styles.helloImAyumi}>Hello, I'm Ayumi Kimura. As a frontend engineer, I'm passionate about web development.</p>
+					<p className={styles.helloImAyumi}> I particularly enjoy crafting user experiences while staying abreast of the latest technologies and design trends.</p>
+				</span>
+			</div>
+		</div>
+	</div>);
 };
 
-export default Tablet;
+export default Tablet;	
